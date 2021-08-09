40fa04068b75355f1bc2104f6c3bc2128791bfc0
SrDri <juanjcarabali@gmail.com>
2021-08-10 04:38:01 +0700

    Update fix task 2

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This script defines a class test_console """
+import unittest
+from console import HBNBCommand
+
+
+class test_console(unittest.TestCase):
+    """ This class defines all test for the console """
+
+    def test_do_create(self):
+        HBNBCommand.do_create(
+            self, 'Place city_id="0001" user_id="0001" \
+name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 \
+price_by_night=300 latitude=37.773972 longitude=-122.431297')
