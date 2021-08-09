0626c51fb2862a9fc03b22e3a7f7604ac66b37a0
SrDri <juanjcarabali@gmail.com>
2021-08-10 06:20:41 +0700

    Update and fix file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            clases = {}
+            for key, val in self.__objects.items():
+                if type(val) == cls:
+                    clases[key] = val
+            return clases
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -36,16 +43,16 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
