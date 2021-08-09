bc24c6175e147f708afb764ff2a69d76127e6440
SrDri <juanjcarabali@gmail.com>
2021-08-10 05:52:56 +0700

    Update test case

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,3 +13,7 @@ class test_console(unittest.TestCase):
             self, 'Place city_id="0001" user_id="0001" \
 name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 \
 price_by_night=300 latitude=37.773972 longitude=-122.431297')
+
+    def create(self):
+        """ create an instance of the HBNBCommand class """
+        return HBNBCommand()
