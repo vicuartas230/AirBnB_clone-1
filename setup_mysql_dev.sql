985f4558d2afc120f235d83b07fb79bd9da5be27
victor <vicuartas230@gmail.com>
2021-08-09 17:02:34 -0500

    Fix syntax in a line of code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- This script prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PROVILEGES ON *.* TO 'hbnb_dev'@'LOCALHOST';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'LOCALHOST';
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
