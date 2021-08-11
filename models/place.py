f7c9b7b95841be6d8dc0d6300da66e70ce7d915a
victor <vicuartas230@gmail.com>
2021-08-11 11:40:00 -0500

    Remove duplicated statement

--- models/place.py
+++ models/place.py
@@ -15,17 +15,6 @@ place_amenity = Table('place_amenity', metadata,
                              ForeignKey("amenities.id"), primary_key=True,
                              nullable=False))
 
-# Table association
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
-
 
 class Place(BaseModel, Base):
     """ A place to stay """
