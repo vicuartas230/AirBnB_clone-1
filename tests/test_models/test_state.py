6e2f5dfe6e8ae189cc4faf2833425974fe5b06db
SrDri <juanjcarabali@gmail.com>
2021-08-12 08:58:39 +0700

    Update test files

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.state import State
+from models import state
+from os import environ
+import models
+import pep8
+import unittest
+
+State = state.State
+storage_t = environ("HBNB_TYPE_STORAGE")
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Test State """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -13,7 +20,11 @@ class test_state(test_basemodel):
         self.name = "State"
         self.value = State
 
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_name_attr(self):
+        """ Test State has attribute name, and it's as an empty string """
+        state = State()
+        self.assertTrue(hasattr(state, "name"))
+        if storage_t == "db":
+            self.assertEqual(state.name, None)
+        else:
+            self.assertEqual(state.name, None)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,10 +2,16 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+from models import user
+from os import environ
+import pep8
+import unittest
+User = user.User
+storage_t = environ("HBNB_TYPE_STORAGE")
 
 
 class test_User(test_basemodel):
-    """ """
+    """ Test User """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -13,22 +19,38 @@ class test_User(test_basemodel):
         self.name = "User"
         self.value = User
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def test_email_attr(self):
+        """ Test User has attr email, and it's an empty string """
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        if storage_t == "db":
+            self.assertEqual(user.email, None)
+        else:
+            self.assertEqual(user.email, None)
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+    def test_password_attr(self):
+        """ Test User has attr password, and it's an empty string """
+        user = User()
+        self.assertTrue(hasattr(user, "password"))
+        if storage_t == "db":
+            self.assertEqual(user.password, None)
+        else:
+            self.assertEqual(user.password, None)
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+    def test_first_name_attr(self):
+        """ Test User has attr first_name, and it's an empty string """
+        user = User()
+        self.assertTrue(hasattr(user, "first_name"))
+        if storage_t == "db":
+            self.assertEqual(user.first_name, None)
+        else:
+            self.assertEqual(user.first_name, None)
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+    def test_last_name_attr(self):
+        """ Test User has attr last_name, and it's an empty string """
+        user = User()
+        self.assertTrue(hasattr(user, "last_name"))
+        if storage_t == "db":
+            self.assertEqual(user.last_name, None)
+        else:
+            self.assertEqual(user.last_name, None)
