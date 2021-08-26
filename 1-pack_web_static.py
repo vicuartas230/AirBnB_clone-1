8c775074444dfdaf5d79cfcee2ce31879b98adeb
victor <vicuartas230@gmail.com>
2021-08-26 10:35:25 -0500

    Change some methods in the code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """ This script defines a function do_pack """
-from fabric.api import local, env
-
-
-env.hosts = [
-    '34.138.131.46',
-    '34.228.168.55'
-]
-
-env.user = "ubuntu"
+from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
-    date = local('date "+%Y%m%d%H%M%S"')
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
     name = 'web_static_' + date + '.tgz'
     local('mkdir -p versions/')
-    local('tar -zcvf versions/{} web_static/'.format(name))
+    descompress = local('tar -zcvf versions/{} web_static'.format(name))
+    if descompress.failed:
+        return None
+    return 'versions/' + name
