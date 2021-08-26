27fb22e570e1e274358729a7e595d0568dadf239
victor <vicuartas230@gmail.com>
2021-08-26 10:14:24 -0500

    Upload script of task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This script sets up your web servers for the deployment of web_static.
+if [[ $(command -v nginx) -ne 0 ]]
+then
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y install nginx
+fi
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -f -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '56 i \\n\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}' /etc/nginx/sites-available/default
+service nginx restart
