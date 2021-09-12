25995fd3e8e237cb3af9757389227992f1a590b0
victor <vicuartas230@gmail.com>
2021-09-11 19:14:42 -0500

    Upload files of task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application """
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states')
+def states():
+    """ This function lists all State objects present in DBStorage """
+    return render_template("9-states.html", cons=storage.all(State), id=None)
+
+
+@app.route('/states/<id>')
+def states_id(id):
+    """ This function lists City objects linked to the state """
+    ids = []
+    for v in storage.all(State).values():
+        ids.append(v.id)
+    return render_template("9-states.html",
+                           cons=storage.all(State), id=id, ids=ids)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ This function removes the current SQLAlchemy Session. """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
